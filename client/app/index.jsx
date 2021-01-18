0ee7a31adbe83c04beebf635b34746ae0d1b7c2e
Kasey <kaseykaufmann@yahoo.com>
2021-01-18 14:41:27 -0500

    Up and Running Now

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -6,7 +6,7 @@ import App from "./components/App/App";
 import NotFound from "./components/App/NotFound";
 import Home from "./components/Home/Home";
 import HelloWorld from "./components/HelloWorld/HelloWorld";
-import "./styles/styles.scss";
+// import "./styles/styles.scss";
 
 render(
   <Router>
